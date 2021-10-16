63c5c3720b26e8d367179943a582d27bbdacd788
Gerald Wodni <gerald.wodni@gmail.com>
2021-10-16 16:42:04 +0200

    emailTemplate: membernumber fixed; successTemplate added

--- views/2021/prices.js
+++ views/2021/prices.js
@@ -9,15 +9,24 @@ module.exports = {
     operatorBankAccount: "HINWEIS: dieser Wert wird über eine Umgebungsvariable bereitgestellt",
     chatRegistration: "HINWEIS: dieser Wert wird über eine Umgebungsvariable bereitgestellt",
 
-    emailTemplate: function _emailTemplate( { prices, values, price } ) {
+    /* message to show after successful registration */
+    successTemplate: function _successTemplate( { price } ) {
+        return `Danke für Ihre Anmeldung!`
+            //+ `\n Bitte überweisen Sie €${price} auf das Konto welches Sie in der Email finden.`
+            + `\n In der Email finden Sie auch Ihren persönlichen 'meineTagung'-Link. Bitte speichern Sie diesen gleich als Bookmark,`
+            + `\n denn mit ihm können Sie an der Tagung teilnehmen und Ihre Präsentationsdetails überarbeiten.`
+            + `\n Die Email erhalten Sie von: ${process.env.SMTP_EMAIL}.`;
+    },
+    /* registration email text body */
+    emailTemplate: function _emailTemplate( { prices, values, price, website } ) {
         return `Hotel: ${values.hotel}\nExtra Tage: ${values.extraDays}\nPreis (gesamt): ${price}`
             + ( price > 0 ? `\nBitte transferieren Sie den vollen Eurobetrag an:\n${prices.operatorBankAccount}` : '' )
             + ( prices.chatRegistration ? `\n\n${process.env.CHAT_REGISTRATION}` : '' )
-            + `\n\nUm an der Konferenz teilzunehmen, und  Ihre Präsentation(en) selbst zu editieren verwenden Sie bitte folgendem Link: https://${k.website}/${prices.myName}/${values.editHash}`
+            + `\n\nUm an der Konferenz teilzunehmen, und  Ihre Präsentation(en) selbst zu editieren verwenden Sie bitte folgendem Link: https://${website}/${prices.myName}/${values.editHash}`
             + `\n\nName: ${values.name}`
             //+ `\nAddress: ${values.address}`
             //+ `\nTelephone: ${values.telephone}`
-            + `\nMitgliesnummer: ${values.email}`
+            + `\nMitgliedsnummer: ${values.memberNumber}`
             + `\nEmail: ${values.email}`
             //+ `\n\nEntourage: ${values.partner}\nName: ${values.partnerName}\nAdresse: ${values.partnerAddress}`
             + `\n\nPräsentation: ${values.presentationTitle} Länge: ${values.presentationLength}\n${values.presentationDescription}`
